2b9155a87785be74a61e1ba35762ba3cbc0fb370
kn-vardhan <kethariv@gmail.com>
2022-04-09 22:56:58 +0530

    Create reset.py

---
+++ reset.py
@@ -0,0 +1,13 @@
+# resetting all the page ranks in the database
+# and starting it afresh
+import sqlite3
+
+connection = sqlite3.connect('spider-data.sqlite')
+cursor = connection.cursor()
+
+cursor.execute('UPDATE Pages SET new_rank=1.0, old_rank=0.0')
+connection.commit()
+
+cursor.close()
+
+print("Ranks of all pages set to 1.0")
